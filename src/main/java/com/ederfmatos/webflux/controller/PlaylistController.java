b8eb063a3374e7dd32c1e840d156a46a9de614c9
ederfmatos <ederfmatos@gmail.com>
2020-04-21 13:30:56 -0300

    Criado RestController com Mono & Flux

---
+++ src/main/java/com/ederfmatos/webflux/controller/PlaylistController.java
@@ -0,0 +1,38 @@
+package com.ederfmatos.webflux.controller;
+
+import com.ederfmatos.webflux.document.Playlist;
+import com.ederfmatos.webflux.services.PlaylistService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController("playlist")
+@RequestMapping("/playlists")
+public class PlaylistController {
+
+    private final PlaylistService service;
+
+    public PlaylistController(PlaylistService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public Flux<Playlist> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Mono<Playlist> findAll(@PathVariable String id) {
+        return service.findById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Playlist> save(@RequestBody Playlist playlist) {
+        return service.save(playlist);
+    }
+
+}
