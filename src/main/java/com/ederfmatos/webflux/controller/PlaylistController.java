537ae1a5344d8159d91608c446ce6a3273442d10
ederfmatos <ederfmatos@gmail.com>
2020-04-21 13:53:59 -0300

    Criado Service reativo funcional

--- src/main/java/com/ederfmatos/webflux/controller/PlaylistController.java
+++ src/main/java/com/ederfmatos/webflux/controller/PlaylistController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-@RestController("playlist")
+//@RestController("playlist")
 @RequestMapping("/playlists")
 public class PlaylistController {
 
---
+++ src/main/java/com/ederfmatos/webflux/handler/PlaylistHandler.java
@@ -0,0 +1,45 @@
+package com.ederfmatos.webflux.handler;
+
+import com.ederfmatos.webflux.document.Playlist;
+import com.ederfmatos.webflux.services.PlaylistService;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.web.reactive.function.BodyInserters.fromPublisher;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+@Component
+public class PlaylistHandler {
+
+    private final PlaylistService service;
+
+    public PlaylistHandler(PlaylistService service) {
+        this.service = service;
+    }
+
+    public Mono<ServerResponse> findAll(ServerRequest request) {
+        return ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(service.findAll(), Playlist.class);
+    }
+
+    public Mono<ServerResponse> findById(ServerRequest request) {
+        String id = request.pathVariable("id");
+
+        return ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(service.findById(id), Playlist.class);
+    }
+
+    public Mono<ServerResponse> save(ServerRequest request) {
+        final Mono<Playlist> playlist = request.bodyToMono(Playlist.class);
+
+        return ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(fromPublisher(playlist.flatMap(service::save), Playlist.class));
+    }
+
+}
